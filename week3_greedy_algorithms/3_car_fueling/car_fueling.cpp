f96a346b6a267a28a1d9bf1ed1633d17db44667f
shubham-sys <subham.kumar032@gmail.com>
2020-05-08 18:59:41 +0530

    Car Fueling In c++

--- week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -1,30 +1,98 @@
 #include <iostream>
 #include <vector>
 
-using std::cin;
-using std::cout;
-using std::vector;
-using std::max;
+// using std::cin;
+// using std::cout;
+// using std::vector;
+// using std::max;
 
-int compute_min_refills(int dist, int tank, vector<int> & stops) {
-    // write your code here
-    return -1;
+/*
+    ********************SAFE MOVE**********************
+    Moving as far as the fuel is good enought to reach another gas station
+*/
+
+using namespace std;
+
+    /*
+        * This program has Loop nested within another loop
+        * So, it seems to have O(n * n) run-time
+        * But, the run time of the program is O(n)
+            => currentRefills can be atmost n - 1
+            => numRefills can be atmost n
+            => So there will be atmost n + 1 operations
+                => O(n + 1) => O(n)
+    */
+
+int minRefills(int n, vector<int> x, int dist_with_full_tank)
+{
+    int numRefills = 0, currentRefills = 0;
+    int lastRefills = 0;
+    //cout<<"Stops where you need to get refilling:";
+    while(currentRefills < (n - 1))
+    {
+        lastRefills = currentRefills;
+        while((currentRefills < (n - 1)) && (x[currentRefills + 1] - x[lastRefills] <= dist_with_full_tank))
+        {
+            currentRefills++;
+            if(currentRefills == (n - 1))
+                break;
+        }
+        //cout<<a[lastRefills]<<'\n';
+        if(currentRefills == lastRefills)
+        // This happens when gas station is far away that even full tank of fuel 
+        // could not make up
+            return -1;
+        if(currentRefills < (n - 1))
+            numRefills++;
+    }
+    return numRefills;
 }
 
-
-int main() {
-    int d = 0;
-    cin >> d;
-    int m = 0;
-    cin >> m;
+int main()
+{
     int n = 0;
-    cin >> n;
+    vector<int> x;
+    int dist_with_full_tank = 0;
 
-    vector<int> stops(n);
-    for (size_t i = 0; i < n; ++i)
-        cin >> stops.at(i);
+    //cout<<"Give n:"
+    cin>>n;
+    int temp = 0;
+    //cout<<"Give gas station stops in kms:";
+    for(int i = 0; i < n; i++)
+    {
+        cin>>temp;
+        x.push_back(temp);
+    }
+    //cour<<"Give Mileage per full tank:";    
+    cin>>dist_with_full_tank;
 
-    cout << compute_min_refills(d, m, stops) << "\n";
-
-    return 0;
+    //cout<<"Number of refilling required:";
+    cout<<minRefills(n, x, dist_with_full_tank)<<'\n';
 }
+
+
+
+
+
+// int compute_min_refills(int dist, int tank, vector<int> & stops) {
+//     // write your code here
+//     return -1;
+// }
+
+
+// int main() {
+//     int d = 0;
+//     cin >> d;
+//     int m = 0;
+//     cin >> m;
+//     int n = 0;
+//     cin >> n;
+
+//     vector<int> stops(n);
+//     for (size_t i = 0; i < n; ++i)
+//         cin >> stops.at(i);
+
+//     cout << compute_min_refills(d, m, stops) << "\n";
+
+//     return 0;
+// }
--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -1,11 +1,56 @@
+"""
+    /*
+        * This program has Loop nested within another loop
+        * So, it seems to have O(n * n) run-time
+        * But, the run time of the program is O(n)
+            => currentRefills can be atmost n - 1
+            => numRefills can be atmost n
+            => So there will be atmost n + 1 operations
+                => O(n + 1) => O(n)
+    */
+"""
+
+"""
+/*
+    ********************SAFE MOVE**********************
+    Moving as far as the fuel is good enought to reach another gas station
+*/
+"""
+
 # python3
 import sys
 
 
-def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+def min_refills(n, a, dist_with_full_tank):
+    num_refills = 0
+    current_refills = 0
+    last_refills = 0
+    #print("Stops where you need to get refilling:")
+    while current_refills < (n - 1):
+        last_refills = current_refills
+        while ((a[current_refills + 1] - a[last_refills]) <= dist_with_full_tank):
+                current_refills = current_refills + 1
+                if current_refills == (n - 1):
+                    break
+        #print(a[last_refills])
+        if current_refills == last_refills:
+            # This happens when gas station is far away that even full tank of 
+            # fuel could not make up
+            return -1
+        if current_refills < (n - 1):
+            num_refills = num_refills + 1
+    return num_refills
 
-if __name__ == '__main__':
-    d, m, _, *stops = map(int, sys.stdin.read().split())
-    print(compute_min_refills(d, m, stops))
+def start():
+    #print("Give n:")
+    n = int(input())
+    #print("Give gas station stops in kms:")
+    a = [int (x) for x in input().split()]
+    assert(len(a) == n)
+    #print("Give Mileage per full tank:")
+    dist_with_full_tank = int(input())
+    #print("Number of refilling required:")
+    print(min_refills(n, a, dist_with_full_tank))
+
+if __name__ == "__main__":
+    start()
