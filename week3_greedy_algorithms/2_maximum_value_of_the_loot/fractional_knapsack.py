7d31939e93fef5e678e48e68c2ec54cb9a4cf114
shubham-sys <subham.kumar032@gmail.com>
2020-05-08 18:59:02 +0530

    Loot in Python

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -4,9 +4,21 @@ import sys
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
-
+    vpw = []
+    for w, v, i in zip(weights, values, list(range(n))):
+        vpw.append((float(v / w), i)) #using tuple for better good
+    vpw = sorted(vpw, key = lambda t: t[0], reverse = True)
+    j = 0
+    while capacity > 0 and j < n:
+        weight = weights[vpw[j][1]]
+        if weight > capacity:
+            value += capacity * vpw[j][0]
+            break
+        else:
+            value += values[vpw[j][1]]
+            capacity -= weight
+            j += 1
     return value
-
 
 if __name__ == "__main__":
     data = list(map(int, sys.stdin.read().split()))
