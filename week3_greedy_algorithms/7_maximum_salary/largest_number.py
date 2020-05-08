9710daead75a52e182a35d7609b46e2e7b32a39f
shubham-sys <subham.kumar032@gmail.com>
2020-05-08 19:02:49 +0530

    Maximum Salary in python

--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -2,11 +2,21 @@
 
 import sys
 
+def isGreatOrEqual(a, b):
+    if a + b > b + a:
+        return True
+    else:
+        return False
 def largest_number(a):
     #write your code here
     res = ""
-    for x in a:
-        res += x
+    while len(a) > 0:
+        maxDigit = '0'
+        for x in a:
+            if isGreatOrEqual(x, maxDigit):
+                maxDigit = x
+        res += maxDigit
+        a.remove(maxDigit)
     return res
 
 if __name__ == '__main__':
