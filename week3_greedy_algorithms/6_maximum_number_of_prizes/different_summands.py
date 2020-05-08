2c84ea80080aba583a8dd241dbc996e91319bd48
shubham-sys <subham.kumar032@gmail.com>
2020-05-08 19:02:06 +0530

    Maximum Prizes in python

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,9 +1,18 @@
 # Uses python3
 import sys
+import pdb
 
 def optimal_summands(n):
     summands = []
     #write your code here
+    sumall = 1
+    start = 1
+    summands.append(start)
+    #pdb.set_trace()
+    while n - sumall > summands[-1]:
+        summands.append(summands[-1] + 1)
+        sumall += summands[-1]
+    summands[-1] += (n - sumall)
     return summands
 
 if __name__ == '__main__':
