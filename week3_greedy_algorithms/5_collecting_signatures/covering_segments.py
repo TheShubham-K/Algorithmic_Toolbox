fc3dcf4c4834fa57242991070a881bf59586b61c
shubham-sys <subham.kumar032@gmail.com>
2020-05-08 19:01:24 +0530

    Collecting Sig. in Python

--- week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
+++ week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
@@ -1,5 +1,6 @@
 # Uses python3
 import sys
+import pdb
 from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
@@ -7,9 +8,15 @@ Segment = namedtuple('Segment', 'start end')
 def optimal_points(segments):
     points = []
     #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+    sortedsegs = sorted(segments, key = lambda t: t[1])
+    #print(sortedsegs)
+    #pdb.set_trace()
+    point = sortedsegs[0].end
+    points.append(point)
+    for s in sortedsegs:
+        if s.start > point:
+            points.append(s.end)
+            point = s.end
     return points
 
 if __name__ == '__main__':
@@ -18,4 +25,6 @@ if __name__ == '__main__':
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
     print(len(points))
-    print(*points)
+    #print(*points)
+    for p in points:
+        print(p, end=' ')
