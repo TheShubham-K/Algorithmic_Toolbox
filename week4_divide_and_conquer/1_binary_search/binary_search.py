aef42468126caaa62996f055cbd14760abe4b1b7
shubham-sys <subham.kumar032@gmail.com>
2020-05-09 15:52:16 +0530

    Binary Search Problem

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -2,8 +2,17 @@
 import sys
 
 def binary_search(a, x):
-    left, right = 0, len(a)
+    left, right = 0, len(a)-1
     # write your code here
+    while left <= right:
+        mid = (left + right) // 2
+        if a[mid] == x:
+            return mid
+        elif a[mid] > x:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +28,5 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
+        #print(linear_search(a, x), end = ' ')
