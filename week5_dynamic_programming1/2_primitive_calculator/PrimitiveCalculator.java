ec7f2e0922d2e39bc41562627ca22a31db5aa963
shubham-sys <subham.kumar032@gmail.com>
2020-05-10 00:11:54 +0530

    Primitive Calculator

--- week5_dynamic_programming1/2_primitive_calculator/PrimitiveCalculator.java
+++ week5_dynamic_programming1/2_primitive_calculator/PrimitiveCalculator.java
@@ -1,8 +1,31 @@
+
 import java.util.*;
 
 public class PrimitiveCalculator {
-    private static List<Integer> optimal_sequence(int n) {
-        List<Integer> sequence = new ArrayList<Integer>();
+    private static int optimal_sequence(int n) {
+        int[] MinNum = new int[n + 1];
+        MinNum[1] = 0;
+        for (int m = 2; m <= n; m++){
+            MinNum[m] = Integer.MAX_VALUE;
+            if (m % 3 == 0){
+                int Num = MinNum[m / 3] + 1;
+                if (Num < MinNum[m]){
+                    MinNum[m] = Num;
+                }
+            } else if (m % 2 == 0){
+                int Num = MinNum[m / 2] + 1;
+                if (Num < MinNum[m]){
+                    MinNum[m] = Num;
+                }
+            }
+            int Num = MinNum[m - 1] + 1;
+            if (Num < MinNum[m]){
+                MinNum[m] = Num;
+                }
+        }
+        return MinNum[n];
+
+        /*List<Integer> sequence = new ArrayList<Integer>();
         while (n >= 1) {
             sequence.add(n);
             if (n % 3 == 0) {
@@ -14,17 +37,18 @@ public class PrimitiveCalculator {
             }
         }
         Collections.reverse(sequence);
-        return sequence;
+        return sequence;*/
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        List<Integer> sequence = optimal_sequence(n);
+        /*List<Integer> sequence = optimal_sequence(n);
         System.out.println(sequence.size() - 1);
         for (Integer x : sequence) {
             System.out.print(x + " ");
-        }
+        }*/
+        System.out.println(optimal_sequence(n));
     }
 }
 
--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,8 +1,10 @@
 # Uses python3
 import sys
 
+
 def optimal_sequence(n):
     sequence = []
+    '''
     while n >= 1:
         sequence.append(n)
         if n % 3 == 0:
@@ -11,7 +13,36 @@ def optimal_sequence(n):
             n = n // 2
         else:
             n = n - 1
+    '''
+    optimalnum = DPoptimal(n)
+    while n >= 1:
+        sequence.append(n)
+        if n % 3 == 0 and optimalnum[n-1] == optimalnum[n // 3 -1] + 1:
+            n = n // 3
+        elif n % 2 == 0 and optimalnum[n-1] == optimalnum[n // 2 -1] + 1:
+            n = n // 2
+        else:
+            n -= 1
     return reversed(sequence)
+
+def DPoptimal(n):
+    optimalnum =n * [0]
+    for i in range(2, n+1):
+        n1, n2, n3 = -1, -1, -1
+        if i % 3 == 0:
+            n3 = optimalnum[i // 3 - 1]
+        if i % 2 == 0:
+            n2 = optimalnum[i // 2 - 1]
+        n1 = optimalnum[i - 1 - 1]
+        optimalnum[i - 1] = findmin(n3, n2, n1) + 1
+    return optimalnum
+
+def findmin(a, b, c):
+    if a == -1:
+        return findmin(c + 1, b, c)
+    if b == -1:
+        return findmin(a, c + 1, c)
+    return min(a, b, c)
 
 input = sys.stdin.read()
 n = int(input)
