398da34ba33424effcf7320beefb7eb6c75d04d8
shubham-sys <subham.kumar032@gmail.com>
2020-05-10 00:08:21 +0530

    Money Change

--- week5_dynamic_programming1/1_money_change_again/ChangeDP.java
+++ week5_dynamic_programming1/1_money_change_again/ChangeDP.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
 
 public class ChangeDP {
-    private static int getChange(int m) {
+    private static int getChange(int money) {
+       int[] coins = {1, 3, 4};
+        int[] MinNumCoins = new int[money+1];
+        MinNumCoins[0] = 0;
+        for (int m = 1; m <= money; m++){
+            MinNumCoins[m] = Integer.MAX_VALUE;
+            for (int i = 0; i < 3; i++){
+                if (m >= coins[i]){
+                    int NumCoins = MinNumCoins[m - coins[i]] + 1;
+                    if (NumCoins < MinNumCoins[m]){
+                        MinNumCoins[m] = NumCoins;
+                    }
+                }
+            }
+        }
         //write your code here
-        return m / 4;
+        return MinNumCoins[money];
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        System.out.println(getChange(m));
+        int money = scanner.nextInt();
+        System.out.println(getChange(money));
 
     }
 }
--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -2,9 +2,18 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m // 4
-
+    min_coins=dict()
+    min_coins[0]=0
+    denom=[1,3,4]
+    for i in range(1,m+1):
+        min_coins[i]=1000000
+        for j in denom:
+            if i>=j:
+                coins=1+min_coins[i-j]
+                if coins<min_coins[i]:
+                    min_coins[i]=coins
+                    
+    return min_coins[m]
 if __name__ == '__main__':
     m = int(sys.stdin.read())
     print(get_change(m))
