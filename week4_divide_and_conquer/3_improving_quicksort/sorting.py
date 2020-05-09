5c0970b967cca260f21792c08e318f8780cef96c
shubham-sys <subham.kumar032@gmail.com>
2020-05-09 15:54:14 +0530

    Improving QuickSort

--- week4_divide_and_conquer/3_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.py
@@ -4,7 +4,17 @@ import random
 
 def partition3(a, l, r):
     #write your code here
-    pass
+    x = a[l]
+    j = l
+    for i in range(l + 1, r +1):
+        if a[i] <= x:
+            j += 1
+            a[i], a[j] = a[j], a[i]
+    a[l], a[j] = a[j], a[l]
+    lj = j
+    while x == a[lj] and lj > -1:
+        lj -= 1
+    return lj + 1, j
 
 def partition2(a, l, r):
     x = a[l]
@@ -23,9 +33,10 @@ def randomized_quick_sort(a, l, r):
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
     #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    #m = partition2(a, l, r)
+    m1, m2 = partition3(a, l, r)
+    randomized_quick_sort(a, l, m1 - 1);
+    randomized_quick_sort(a, m2 + 1, r);
 
 
 if __name__ == '__main__':
