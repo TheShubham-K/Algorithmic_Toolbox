8e202b98919ce1c6b21d13e2a28879b5201e5fe4
shubham-sys <subham.kumar032@gmail.com>
2020-05-10 00:12:15 +0530

    Editing Distance

--- week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,23 @@
 # Uses python3
 def edit_distance(s, t):
     #write your code here
-    return 0
+    distance = [[0 for col in range(len(s) + 1)] for row in range(len(t) + 1)]
+    #print(distance)
+    for i in range(len(s) + 1):
+        distance[0][i] = i
+    for i in range(len(t) + 1):
+        distance[i][0] = i
+    for m in range(1, len(s) + 1):
+        for n in range(1, len(t) + 1):
+            ins = distance[n][m-1] + 1
+            de = distance[n-1][m] + 1
+            mat = distance[n-1][m-1]
+            mis = distance[n-1][m-1] + 1
+            if s[m-1] == t[n-1]:
+                distance[n][m] = min(ins, de, mat)
+            else:
+                distance[n][m] = min(ins, de, mis)
+    return distance[-1][-1]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
