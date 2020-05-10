6ae194fac9f5e5cdfcd9fec07ab7bac666d0a7a3
shubham-sys <subham.kumar032@gmail.com>
2020-05-10 18:49:24 +0530

    Gold Problem Solved in Python

--- week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
@@ -1,15 +1,45 @@
 import java.util.*;
 
+
 public class Knapsack {
-    static int optimalWeight(int W, int[] w) {
-        //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+    static int max(int a, int b)
+    {
+        return (a > b)? a : b;
+    }
+
+    static int optimalWeight(int n, int W, int[] wt) {
+        int[][] K = new int[n+1][W+1];
+        for (int i = 0; i <= n; i++){
+            for (int w = 0; w <= W; w++){
+                if (i == 0 || w == 0)
+                    K[i][w] = 0;
+                else if (wt[i-1] <= w)
+                    K[i][w] = max(wt[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+                else
+                    K[i][w] = K[i-1][w];
+            }
         }
-        return result;
+        return K[n][W];
+        /*int[][] value = new int[W+1][n+1];
+        for (int i = 0; i <= n; i++){
+            value[0][i] = 0;
+        }
+        for (int ww = 0; ww <= W; ww++){
+            value[ww][0] = 0;
+        }
+        for (int i = 1; i <= n; i++){
+            for (int ww = 1; ww <= W; ww++){
+                value[ww][i] = value[ww][i-1];
+                if (w[i] <= ww){
+                    int val = value[ww-1][i-1] + w[i];
+                    if (value[ww][i] < val){
+                        value[ww][i] = val;
+                    }
+                }
+            }
+        }
+        return value[W][n];*/
+        //write you code here
     }
 
     public static void main(String[] args) {
@@ -17,11 +47,40 @@ public class Knapsack {
         int W, n;
         W = scanner.nextInt();
         n = scanner.nextInt();
-        int[] w = new int[n];
+        int[] wt = new int[n+1];
         for (int i = 0; i < n; i++) {
-            w[i] = scanner.nextInt();
+            wt[i] = scanner.nextInt();
         }
-        System.out.println(optimalWeight(W, w));
+        //w[0] = 0;
+        System.out.println(optimalWeight(n, W, wt));
     }
 }
+
+
+
+
+// public class Knapsack {
+//     static int optimalWeight(int W, int[] w) {
+//         //write you code here
+//         int result = 0;
+//         for (int i = 0; i < w.length; i++) {
+//           if (result + w[i] <= W) {
+//             result += w[i];
+//           }
+//         }
+//         return result;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         int W, n;
+//         W = scanner.nextInt();
+//         n = scanner.nextInt();
+//         int[] w = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             w[i] = scanner.nextInt();
+//         }
+//         System.out.println(optimalWeight(W, w));
+//     }
+// }
 
--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -3,11 +3,17 @@ import sys
 
 def optimal_weight(W, w):
     # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    n = len(w)
+    value = [[0 for col in range(W + 1)] for row in range(n + 1)]
+    for i in range(1, n + 1):
+        for cp in range(1, W + 1):
+            value[i][cp] = value[i-1][cp]
+            if w[i-1] <= cp:
+                val = value[i-1][cp - w[i-1]] + w[i - 1]
+                if val > value[i][cp]:
+                    value[i][cp] = val
+#    print(value)
+    return value[-1][-1]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
