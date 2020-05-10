9590edbd4619a135f365b64a8b59cc0e1cb43971
shubham-sys <subham.kumar032@gmail.com>
2020-05-10 18:53:47 +0530

    Lcs2

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -3,10 +3,60 @@
 
 using std::vector;
 
+
 int lcs2(vector<int> &a, vector<int> &b) {
   //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+  vector<int> result;
+  int m = a.size();
+  int n = b.size();
+  int t = max(m,n);
+  int D[m+1][n+1];
+  for(int i=0;i<=m;++i) D[i][0]=i;
+  for(int i=0;i<=n;++i) D[0][i]=i;
+  for(int i=1;i<=m;++i)
+  	for(int j=1;j<=n;++j)
+  	{
+  		int op[3]={t,t,t}; //t is equal to infinite 
+  		op[0] = D[i-1][j]+1;
+  		op[1] = D[i][j-1]+1;
+  		if(a[i-1]==b[j-1]) op[2] = D[i-1][j-1];
+  		else op[2] = D[i-1][j-1] + 1; 
+  		D[i][j] = min(op[1],op[0]);
+  		D[i][j] = min(D[i][j],op[2]);
+	}
+  	//Backward
+  	int i = m;
+  	int j = n;
+  	while(i!=0&&j!=0)
+  	{
+  		if(D[i][j]==D[i-1][j-1]&&D[i][j]<=D[i][j-1]&&D[i][j]<=D[i-1][j]) 
+		{
+		  result.push_back(a[j-1]);
+		  i--;
+		  j--;
+		  continue;	
+		}
+		if(D[i-1][j]<=D[i][j-1])
+		{
+			i--;
+			continue;
+		}
+		if(D[i][j-1]<=D[i-1][j])
+		{
+			j--;
+			continue;
+		}
+	}
+  return result.size();
 }
+
+
+
+
+// int lcs2(vector<int> &a, vector<int> &b) {
+//   //write your code here
+//   return std::min(std::min(a.size(), b.size()), c.size());
+// }
 
 int main() {
   size_t n;
--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,22 +1,20 @@
-import sys
+import sys, random, time
 
 def lcs2(a, b):
+    m =len(a)
+    n =len(b)
     #write your code here
-    L = [[0 for x in range(len(b) + 1)] for x in range(len(a) + 1)]
+    L = [[0 for x in range(n + 1)] for x in range(m + 1)]
     ins = [0]*4
-    for i in range(len(a)):
-        for j in range(len(b)):
-            ins[0] = L[i-1][j]
-            ins[1] = L[i][j-1]
-            mis    = L[i-1][j-1]
-            mat    = L[i-1][j-1] + 1
+    for i in range(m+1):
+        for j in range(n+1):
             if a[i-1] == b[j-1]:
-                L[i][j] = max(max(ins), mat)
+                L[i][j] = L[i-1][j-1] + 1
             else:
-                L[i][j] = max(ins)
-    return L[i][j]
-
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+    return L[m][n]
 if __name__ == '__main__':
+    
     input = sys.stdin.read()
     data = list(map(int, input.split()))
 
@@ -30,3 +28,22 @@ if __name__ == '__main__':
     b = data[:m]
 
     print(lcs2(a, b))
+   
+   
+#    an = random.randint(0, 100)
+#    a =[]
+#    for i in range(an):
+#        a.append(random.randint(0, 100))
+#    bn = random.randint(30, 100)
+#    b =[]
+#    for i in range(bn):
+#        b.append(random.randint(0, 100))
+#    start = time.time()
+#    print(a)
+#    print(b)
+#    result = lcs2(a, b)
+#    end = time.time()
+#    print(result)
+#    print('the running time is %0.5f s' % (end-start))   
+    
+    
