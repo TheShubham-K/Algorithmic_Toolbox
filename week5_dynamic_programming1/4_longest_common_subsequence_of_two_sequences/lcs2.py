c0023caf5c2aa732ecf153a737c3c404ac953df8
shubham-sys <subham.kumar032@gmail.com>
2020-05-13 21:33:11 +0530

    LCS2 Worked In Python 3

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,54 +1,31 @@
+# Uses python3
+
 import sys
-import random
-import time
 
 
-def lcs2(m, wt, val, n):
+def lcs2(a, b):
     # write your code here
-    L = [[None]*(n + 1) for x in range(m + 1)]
-    for i in range(m+1):
-        for j in range(n+1):
-            if i == 0 or j == 0:
-                L[i][j] = 0
-            elif val[i-1] == wt[j-1]:
-                L[i][j] = L[i-1][j-1] + 1
+    result = [[0 for j in range(m+1)] for i in range(n + 1)]
+    for x in range(1, n + 1):
+        for y in range(1, m + 1):
+            if a[x - 1] == b[y - 1]:
+                result[x][y] = result[x - 1][y - 1] + 1
             else:
-                L[i][j] = max(L[i-1][j], L[i][j-1])
-    return L[n][m]
+                result[x][y] = max(result[x - 1][y], result[x][y - 1])
+    return result
 
 
 if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
 
-    n = int(input())
-    val = list(map(int, input().split()))
-    m = int(input())
-    wt = list(map(int, input().split()))
-    print(lcs2(m, wt, val, n))
-    # input = sys.stdin.read()
-    # data = list(map(int, input.split()))
+    n = data[0]
+    data = data[1:]
+    a = data[:n]
 
-    # n = data[0]
-    # data = data[1:]
-    # a = data[:n]
-
-    # data = data[n:]
-    # m = data[0]
-    # data = data[1:]
-    # b = data[:m]
-
-
-#    an = random.randint(0, 100)
-#    a =[]
-#    for i in range(an):
-#        a.append(random.randint(0, 100))
-#    bn = random.randint(30, 100)
-#    b =[]
-#    for i in range(bn):
-#        b.append(random.randint(0, 100))
-#    start = time.time()
-#    print(a)
-#    print(b)
-#    result = lcs2(a, b)
-#    end = time.time()
-#    print(result)
-#    print('the running time is %0.5f s' % (end-start))
+    data = data[n:]
+    m = data[0]
+    data = data[1:]
+    b = data[:m]
+    result = lcs2(a, b)
+    print(result[n][m])
