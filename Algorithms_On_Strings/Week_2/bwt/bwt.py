87b302aca73204fc78d60cc41a801c330f5e14cc
shubham-sys <subham.kumar032@gmail.com>
2020-06-10 12:54:38 +0530

    BWT Matching

---
+++ Algorithms_On_Strings/Week_2/bwt/bwt.py
@@ -0,0 +1,19 @@
+# python3
+import sys
+
+
+def BWT(text):
+    l = len(text)
+    M = []
+    for i in range(1):
+        M.append(text[i:] + text[:i])
+    M.sort()
+    result = ""
+    for row in M:
+        result += row[-1]
+    return result
+
+
+if __name__ == '__main__':
+    text = sys.stdin.readline().strip()
+    print(BWT(text))
